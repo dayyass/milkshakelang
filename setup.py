4c1f3ddc9d166ff4a4f40f58e9cb4fbd9c4a23f1
Dani El-Ayyass <dayyass@yandex.ru>
2022-08-18 00:52:42 +0300

    add setup py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+from milkshakelang import __version__
+
+with open("README.md", mode="r", encoding="utf-8") as fp:
+    long_description = fp.read()
+
+
+setup(
+    name="milkshakelang",
+    version=__version__,
+    description="The MilkShake Programming Language.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Dani El-Ayyass",
+    author_email="dayyass@yandex.ru",
+    license_files=["LICENSE"],
+    url="https://github.com/dayyass/milkshakelang",
+    packages=["milkshakelang"],
+)
